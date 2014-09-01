bd3509a07403553c2271337acd897da69bc5c08c
Juan Pablo Pinilla Ossa <jpinilla@climate.com>
2014-08-31 21:10:48 -0500

    Test docx rels images

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 var Handlebars = require('handlebars');
 var JSZip = require('jszip');
 var fs = require('fs');
+var xmldoc = require('xmldom');
+var DOMParser = xmldoc.DOMParser;
+var XMLSerializer = xmldoc.XMLSerializer;
+var domParser = new DOMParser();
+var xmlSerializer = new XMLSerializer();
 
 var templateData = require('./demo/data').data;
 
 fs.readFile('demo/template.docx', function(erorr, data) {
   var zip = new JSZip(data);
-  var templateText = zip.file('word/document.xml').asText();
-  var template = Handlebars.compile(templateText);
+  var documentText = zip.file('word/document.xml').asText();
+  var relationsText = zip.file('word/_rels/document.xml.rels').asText();
+  var template = Handlebars.compile(documentText);
   var compiled = template(templateData);
 
   zip.file('word/document.xml', compiled);
 
-  // @todo urls and images, check document.xml.rels
+
+  var xml = domParser.parseFromString(relationsText, 'text/xml');
+  var root = xml.getElementsByTagName('Relationships')[0];
+
+  // demo imgs
+  for (var i=1; i<10; i++) {
+    root.appendChild(createImage(xml, i));
+  }
+
+  zip.file('word/_rels/document.xml.rels', xmlSerializer.serializeToString(xml));
+
 
   var buffer = zip.generate({type: 'nodebuffer'});
   fs.writeFile('demo/out_template.docx', buffer);
 });
+
+
+function createImage(xml, i) {
+  var relation = xml.createElement('Relationship');
+  relation.setAttribute('Target', 'media/image01.jpg');
+  relation.setAttribute('Type', 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/image');
+  relation.setAttribute('Id', 'dId'+i);
+
+  return relation;
+}
